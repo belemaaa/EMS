4fb6144e8aa5a0829b27f3d649e428c0c026eac3
belemaaa <graceitamunoala@gmail.com>
2023-10-02 21:35:13 +0000

    signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div>
+        <p>EMS</p>
+        <div>
+            <p>Admin Signup</p>
+            <form method='POST'>
+                <input typeof='text' placeholder='first name'/>
+                <input typeof='text' placeholder='last name'/>
+                <input typeof='text' placeholder='Username'/>
+                <input typeof='password' placeholder='Password'/>
+                <input typeof='email' placeholder='email'/>
+                <input typeof='text' placeholder='phone'/>
+
+                <button typeof='submit'>Sign up</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
