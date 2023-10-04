918f1dc1b706b8037b5ae331428aac268406be41
belemaaa <graceitamunoala@gmail.com>
2023-10-04 23:57:51 +0000

    signup base

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,35 +5,34 @@ import {BiSolidLockAlt} from 'react-icons/bi'
 
 const Login = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <div className=''>
-        <div className='items-center justify-center text-center'>
-          <SiGoogletagmanager size={40} className='text-center justify-between'/>
-        </div>
-        <div className='shadow-lg rounded-lg my-6 text-center w-[600px] border'>
-          <p className='my-6 text-xl font-semibold text-gray-600'>Welcome</p>
-          <form method='POST'>
-            <div className='flex flex-row mx-6 border'>
-              <input typeof='text' placeholder='Username' className='w-[500px] bg-blue-50 rounded-[0.150rem] text-black p-3 placeholder:text-gray-400'/> <br/>
-              <div className='bg-gray-300 w-14'>
-                <BsFillPersonFill className='text-[#353638] mx-3 my-3' size={30}/>
-              </div>
-            </div>
-
-            <div className='flex flex-row mx-6 border'>
-              <input typeof='text' placeholder='Password' className='w-[500px] bg-blue-50 rounded-[0.150rem] text-black p-3 placeholder:text-gray-400'/> <br/>
-              <div className='bg-gray-300 w-14'>
-                <BiSolidLockAlt className='text-[#353638] mx-3 my-3' size={30}/>
-              </div>
-            </div>
-
-            <div className='w-16 bg-blue-500 rounded-[1.150rem] hover:bg-blue-600'>
-              <button typeof='submit' className='text-center text-white'>Log in</button>
-            </div>
-            
-          </form>
-        </div>
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <div className='mb-6'>
+        <SiGoogletagmanager size={60}/>
       </div>
+      <div className='shadow-lg rounded-lg my-6 text-center w-[550px] border'>
+        <p className='my-6 text-xl font-semibold text-gray-600'>Welcome</p>
+        <form method='POST'>
+          <div className='flex flex-row mx-6 border'>
+            <input typeof='text' placeholder='Username' className='w-[500px] rounded-[0.150rem] text-black px-3 py-0 placeholder:text-gray-400'/> <br/>
+            <div className='bg-gray-300 w-14'>
+              <BsFillPersonFill className='text-[#353638] mx-4 my-3' size={25}/>
+            </div>
+          </div>
+
+          <div className='flex flex-row mx-6 border mt-4'>
+            <input typeof='text' placeholder='Password' className='w-[500px] rounded-[0.150rem] text-black p-3 placeholder:text-gray-400'/> <br/>
+            <div className='bg-gray-300 w-14'>
+              <BiSolidLockAlt className='text-[#353638] mx-4 my-3' size={25}/>
+            </div>
+          </div>
+
+          <div className='w-[500px] bg-blue-500 hover:bg-blue-600 m-6 duration-500'>
+            <button typeof='submit' className='text-center text-white py-2 text-lg'>Log in</button>
+          </div>
+          
+        </form>
+      </div>
+
         
     </div>
   )
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
+import {SiGoogletagmanager} from 'react-icons/si'
+import {BsFillPersonFill} from 'react-icons/bs'
+import {BiSolidLockAlt} from 'react-icons/bi'
 
 const Signup = () => {
   return (
-    <div>
-        <p>EMS</p>
-        <div>
-            <p>Admin Signup</p>
-            <form method='POST'>
-                <input typeof='text' placeholder='first name'/>
-                <input typeof='text' placeholder='last name'/>
-                <input typeof='text' placeholder='Username'/>
-                <input typeof='password' placeholder='Password'/>
-                <input typeof='email' placeholder='email'/>
-                <input typeof='text' placeholder='phone'/>
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <div className='mb-6'>
+        <SiGoogletagmanager size={60}/>
+      </div>
+      <div className='shadow-lg rounded-lg my-6 text-center w-[550px] border'>
+        <p className='my-6 text-xl font-semibold text-gray-600'>Welcome</p>
+        <form method='POST'>
+          <div className='flex flex-row mx-6 border'>
+            <input typeof='text' placeholder='Username' className='w-[500px] rounded-[0.150rem] text-black px-3 py-0 placeholder:text-gray-400'/> <br/>
+            <div className='bg-gray-300 w-14'>
+              <BsFillPersonFill className='text-[#353638] mx-4 my-3' size={25}/>
+            </div>
+          </div>
 
-                <button typeof='submit'>Sign up</button>
-            </form>
-        </div>
+          <div className='flex flex-row mx-6 border mt-4'>
+            <input typeof='text' placeholder='Password' className='w-[500px] rounded-[0.150rem] text-black p-3 placeholder:text-gray-400'/> <br/>
+            <div className='bg-gray-300 w-14'>
+              <BiSolidLockAlt className='text-[#353638] mx-4 my-3' size={25}/>
+            </div>
+          </div>
+
+          <div className='w-[500px] bg-blue-500 hover:bg-blue-600 m-6 duration-500'>
+            <button typeof='submit' className='text-center text-white py-2 text-lg'>Log in</button>
+          </div>
+          
+        </form>
+      </div>
+
+        
     </div>
   )
 }
