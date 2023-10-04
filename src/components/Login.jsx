8e99724ca64735bd7c272a7595b0eb42568e542a
belemaaa <graceitamunoala@gmail.com>
2023-10-04 23:36:59 +0000

    additions to login ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,29 +7,31 @@ const Login = () => {
   return (
     <div className='flex justify-center items-center h-screen'>
       <div className=''>
-        <div className='items-center justify-center'>
-          <SiGoogletagmanager size={40} className='text-center justify-center items-center'/>
+        <div className='items-center justify-center text-center'>
+          <SiGoogletagmanager size={40} className='text-center justify-between'/>
         </div>
-        <div className='shadow-lg rounded-lg my-6 py-5 text-center w-fit'>
-            <p>Welcome</p>
-            <form method='POST'>
-              <div className='flex flex-row'>
-                <input typeof='text' placeholder='Username' className='w-16 bg-blue-200 rounded-[0.150rem] text-black p-3 placeholder:text-gray-400'/> <br/>
-                <div className='bg-gray-400'>
-                  <BsFillPersonFill className='text-[#353638]'/>
-                </div>
-                
+        <div className='shadow-lg rounded-lg my-6 text-center w-[600px] border'>
+          <p className='my-6 text-xl font-semibold text-gray-600'>Welcome</p>
+          <form method='POST'>
+            <div className='flex flex-row mx-6 border'>
+              <input typeof='text' placeholder='Username' className='w-[500px] bg-blue-50 rounded-[0.150rem] text-black p-3 placeholder:text-gray-400'/> <br/>
+              <div className='bg-gray-300 w-14'>
+                <BsFillPersonFill className='text-[#353638] mx-3 my-3' size={30}/>
               </div>
-              <div className='flex flex-row'>
-                <input typeof='password' placeholder='Password' className='w-16 bg-blue-200 rounded-[0.150rem] text-black p-3 placeholder:text-gray-400'/> <br/>
-                <BiSolidLockAlt className='text-[#353638]'/>
-              </div>
+            </div>
 
-              <div className='w-16 bg-blue-500 rounded-[1.150rem] hover:bg-blue-600'>
-                <button typeof='submit' className='text-center text-white'>Log in</button>
+            <div className='flex flex-row mx-6 border'>
+              <input typeof='text' placeholder='Password' className='w-[500px] bg-blue-50 rounded-[0.150rem] text-black p-3 placeholder:text-gray-400'/> <br/>
+              <div className='bg-gray-300 w-14'>
+                <BiSolidLockAlt className='text-[#353638] mx-3 my-3' size={30}/>
               </div>
-              
-            </form>
+            </div>
+
+            <div className='w-16 bg-blue-500 rounded-[1.150rem] hover:bg-blue-600'>
+              <button typeof='submit' className='text-center text-white'>Log in</button>
+            </div>
+            
+          </form>
         </div>
       </div>
         
